e11c42383ae46b9de39f51f64a4a2ffc547ad608
Bioły <41907457+Biolasek@users.noreply.github.com>
2022-03-28 16:19:34 +0200

    .

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # dpay-payments
 Unofficial API for dpay polish payments.
+
+### Installation
+```
+npm i dpay-payments
+```
+
+### Sample usage
+*Creating payment*
+```js
+const dpay = require('dpay-payments');
+const dpayObj = new dpay("Service name", "Secret hash"/*, production (default true)*/);
+[...]
+test();
+
+async function test() {
+    const res = await dpayObj.createPayment("1.00", "https://github.com/Biolasek/success", "https://github.com/Biolasek/fail", "https://github.com/Biolasek/ipn");
+
+    /**
+     * Response on success:
+     * { status: true, error: false, msg: 'here's the link to payment so you can easly grab this', transactionId: 'id of transaction' }
+     *
+     * More information here in "Response samples" : https://docs.dpay.pl/#operation/registerPayment
+    */
+    console.log(res);
+}
+```
+
+*Get info about transaction*
+```js
+[...]
+async function test() {
+    [...]
+    sthother(res.transactionId);
+}
+async function sthother(transaction_id) {
+    const information = await dpayObj.getPaymentInfo(transaction_id);
+
+    /**
+     * Response on success:
+     * { status: 'success' transaction: { some other informations including paid status }}
+     * More information here in "Response samples" : https://docs.dpay.pl/#operation/transactionDetails
+    */
+    console.log(information);
+}
+```
--- index.js
+++ index.js
@@ -4,21 +4,44 @@ class dpay {
     constructor(service, hash, production = true) {
         this.service = service;
         this.hash = hash;
-        this.url = production ? "https://secure.dpay.pl/register":"https://secure-test.dpay.pl/register";
+        this.paymentUrl = production ? "https://secure.dpay.pl/register/":"https://secure-test.dpay.pl/register/";
+        this.paymentInfoUrl = production ? "https://panel.dpay.pl/api/v1/pbl":"https://panel.digitalpayments.pl/api/v1/pbl";
     }
 
-    async createPayment(value, url_success, url_fail, url_ipn, installment = true, creditcard = true, paysafecard = true, paypal = true, nobanks = false, channel = "", email = "", client_name = "", client_surname = "", accept_tos = 1, description = "Example description 123", custom = "your custom data", style = "default") {
+    async createPayment(value, url_success, url_fail, url_ipn, installment, creditcard, paysafecard, paypal, nobanks, channel, email, client_name, client_surname, accept_tos, description, custom, style) {
         const checksum = sha256(`${this.service}|${this.hash}|${value}|${url_success}|${url_fail}|${url_ipn}`);
-        const query = JSON.stringify({ service: this.service, value, url_success, url_fail, url_ipn, checksum/*, installment, creditcard, paysafecard, paypal, nobanks, channel, email, client_name, client_surname, accept_tos, description, custom, style*/ });
+        const query = JSON.stringify({ service: this.service, value, url_success, url_fail, url_ipn, checksum, installment, creditcard, paysafecard, paypal, nobanks, channel, email, client_name, client_surname, accept_tos, description, custom, style });
         try {
-            const res = await axios.post(this.url, query);
-            console.log(res);
-            console.log(`${this.service}|${this.hash}|${value}|${url_success}|${url_fail}|${url_ipn}`);
-            console.log(query);
+            const res = await axios.post(this.paymentUrl, query);
             return res.data;
         } catch (err) {
             return JSON.stringify({ status: false, error: true, msg: `${err.response.status} - ${err.response.statusText}` });
         }
     }
+
+    async getPaymentInfo(transaction_id) {
+        const checksum = sha256(`${this.service}|${transaction_id}|${this.hash}`);
+        const query = { service: this.service, transaction_id, checksum };
+        try {
+            const res = await axios.post(`${this.paymentInfoUrl}/details`, query);
+            return res.data;
+        } catch (err) {
+            console.log(err.response?.data.message);
+        }
+    }
+
+    // async refundPayment(transaction_id) {
+    //     const checksum = sha256(`${this.service}|${transaction_id}|${this.hash}`);
+    //     const query = { service: this.service, transaction_id, checksum };
+    //     try {
+    //         const res = await axios.post(`${this.paymentInfoUrl}/refund`, query);
+    //         console.log(res);
+    //         return res.data;
+    //     } catch (err) {
+    //         console.log(err.response);
+    //         console.log("error");
+    //         console.log(checksum);
+    //     }
+    // }
 }
 module.exports = dpay;
